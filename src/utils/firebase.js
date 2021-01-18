1a0a057b76acb4c9e027908bf9f508edf1990085
Vivi Lin <vivi2393142@gmail.com>
2021-01-18 09:57:55 +0800

    Reset git ignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,18 +3,9 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 import { nanoid } from 'nanoid';
+import { firebaseConfig } from './firbaseConfig';
 
 // init firebase
-const firebaseConfig = {
-    apiKey: 'AIzaSyDtNmp8oroSpowKQmr4nHAgqsJ59VCafYE',
-    authDomain: 'pica-b4a59.firebaseapp.com',
-    databaseURL: 'https://pica-b4a59.firebaseio.com',
-    projectId: 'pica-b4a59',
-    storageBucket: 'pica-b4a59.appspot.com',
-    messagingSenderId: '897821043480',
-    appId: '1:897821043480:web:cf4c3d4eba424800d7cc6d',
-    measurementId: 'G-R544SCV03H',
-};
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const userDb = db.collection('userData');
